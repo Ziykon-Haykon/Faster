1feec600395935e673dc03023e2d3d7534c59ca2
ziykon <zike.haykon1x1@gmail.com>
2026-01-12 09:06:36 +0500

    add styles, function say_hello has return html file

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 from pydantic import BaseModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/hello", response_class=HTMLResponse)
-def say_hello(name: str = Form(...), age: int = Form(...)):
-    return f"<h2>Hi, {name}, your age is {age}!<h2>"
+def say_hello(request: Request, name: str = Form(...), age: int = Form(...)):
+    return templates.TemplateResponse(
+        "hello.html", 
+        {
+            "request": request, 
+            "name": name,
+            "age": age
+        })
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
