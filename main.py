d4b9ad1d6b8a74b841c928eb804b8277cd4c68ab
ziykon <zike.haykon1x1@gmail.com>
2026-01-12 08:26:49 +0500

    sorting files, add uvicorn.run launch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import uvicorn
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,3 +14,6 @@ def index(request: Request):
 @app.post("/hello", response_class=HTMLResponse)
 def say_hello(name: str = Form(...), age: int = Form(...)):
     return f"<h2>Hi, {name}, your age is {age}!<h2>"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
